0a29f1db53a617e95c619dbb86d84412c815a3d6
Aleksa Lukic <lukicaleksa04@gmail.com>
2021-08-07 11:14:36 +0200

    Fix bugs

--- src/server/entity/Location.ts
+++ src/server/entity/Location.ts
@@ -1,8 +1,18 @@
-import {Entity, PrimaryGeneratedColumn} from "typeorm";
+import {Column, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm";
+import {Mark} from "./Mark";
 
 @Entity()
 export class Location {
 
-    @PrimaryGeneratedColumn({ type: "int", name: "id" })
+    @PrimaryGeneratedColumn({type: "int", name: "id"})
     public id: number;
+
+    @Column()
+    public name: string;
+
+    @OneToMany(
+        () => Mark,
+        (mark) => mark.idLocation
+    )
+    public listOfMarks!: Mark[];
 }
--- src/server/entity/Mark.ts
+++ src/server/entity/Mark.ts
@@ -1,7 +1,20 @@
-import {Entity, PrimaryGeneratedColumn} from "typeorm";
+import {Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
+import {Location} from "./Location";
 
 @Entity()
 export class Mark {
     @PrimaryGeneratedColumn({type: "int", name: "id"})
     public id: number;
+
+    @Column()
+    public name: string;
+
+    @Column("int")
+    @ManyToOne(
+        () => Location,
+        (location) => location.listOfMarks,
+        {onDelete: "NO ACTION", onUpdate: "NO ACTION"}
+    )
+    @JoinColumn([{name: "id_location", referencedColumnName: "id"}])
+    public idLocation: Location;
 }
